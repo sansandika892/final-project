c5700405910fe44a8d8738bcdecf5ef2509a5718
sandika ramadana pasaribu <sansandika892@gmail.com>
2026-04-21 13:43:57 +0700

    day 14 template

---
+++ resources/views/admin/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.master')
+
+
+
+<div class="mb-5">
+    <h1 class="mb-3 h2">Blank</h1>
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ Route::get('/', function () {
     return view('landing.pages.index');
 });
 
+Route::get('/', function () {
+    return view('admin.pages.index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,6 +21,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.master');
+    })->name('admin.index');
 });
 
 require __DIR__.'/auth.php';
